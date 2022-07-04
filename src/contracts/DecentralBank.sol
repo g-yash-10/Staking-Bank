f8df57b31cef5e3822ebd07259b19ddc09e1d5e2
g-yash-10 <cometyash48@gmail.com>
2022-07-04 18:40:12 +0530

    Reward contract deployed

--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,9 +1,7 @@
-pragma solidity ^0.5.0;
+pragma solidity '^0.5.0';
 
-contract Tether
-{
-    string public name = 'Tether';
-    string piblic symbol = 'USDT';
-    uint256 public totalSupply = 1000000000000000000000000; //1 million tokens
-    unit8 public decimals = 18;
+contract DecentralBank{
+    string public name = 'Decentral Bank'   ;
+    address public owner;
+    
 }
