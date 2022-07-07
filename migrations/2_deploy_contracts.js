dab40727a9f249189a23da3eafeb70f4b3cd68ac
g-yash-10 <cometyash48@gmail.com>
2022-07-07 21:39:33 +0530

    deployers added in migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,16 @@
 const Tether = artifacts.require('Tether');
 const RWD = artifacts.require('RWD');
 const DecentralBank = artifacts.require('DecentralBank');
-module.exports = async function()
+module.exports = async function(deployer, network, accounts)
 {
-    await deployer.deploy(Tether),
-    await deployer.deploy(RWD);
-    await deployer.deploy(DecentralBank);
+    await deployer.deploy(Tether)
+    const tether = await Tether.deployed()
+    await deployer.deploy(RWD)
+    const rwd = await RWD.deployed()
+
+    await deployer.deploy(DecentralBank)
+    const decentralBank = await DecentralBank.deployed()
+
+    await rwd.transfer(DecentralBank.address, '100000000000000000000000')
+    await tether.transfer(accounts[1], '100000000000000000')
 };
--- src/contracts/DecentralBank.sol
+++ src/contracts/DecentralBank.sol
@@ -1,7 +1,13 @@
 pragma solidity '^0.5.0';
-
+import './RWD';
+import './Tether';
 contract DecentralBank{
     string public name = 'Decentral Bank'   ;
     address public owner;
-    
+    RWD public rwd;
+    Tether public  tether;
+    constructor(RWD _rwd, Tether _tether) public{
+        rwd = _rwd;
+        tether = _tether;
+    }
 }
